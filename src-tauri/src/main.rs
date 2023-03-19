7b5eefa249035ff76b889ac3004e647d966f1ff6
EvgenKot <ukal.evgen@gmail.com>
2023-03-19 18:47:21 +0500

    mission bgn

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,30 +1,51 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-struct PolygonCoordinates {
-    vertices: Vec<Vec<[f64; 2]>>,
-}
-
+use rusqlite::{Connection, Result};
+mod mission;
+use mission::{camera::Camera, uav::Uav};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-   format!("Hello, {}!", name)
-}
-
-
-#[tauri::command]
-fn receive_polygon_coordinates(vertices: Vec<Vec<[f64; 2]>>) {
-    println!("Received polygon coordinates: {:?}", vertices);
+    format!("Hello, {}!", name)
 }
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet, receive_polygon_coordinates])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let conn = Connection::open("mydatabase.db").expect("Cant open base");
+
+    mission::uav::Uav::sql_create_table(&conn).expect("cant create uav table");
+    mission::camera::Camera::sql_create_table(&conn).expect("Cant create camera table");
+
+    match Uav::get_uavs(&conn) {
+        Ok(uavs) => {
+            for uav in uavs {
+                println!("UAV: {:?}", uav);
+            }
+        }
+        Err(err) => {
+            eprintln!("Error fetching UAVs: {}", err);
+        }
+    }
+
+
+    // let camera_iter = Camera::sql_get_cameras(&conn).unwrap();
+
+    // for camera_item in camera_iter {
+    //     match camera_item {
+    //         Ok(camera) => camera.print_camera(),
+    //         Err(_) => println!("err =("),
+    //     }
+    // }
+
+
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            mission::receive_polygon_coordinates
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
